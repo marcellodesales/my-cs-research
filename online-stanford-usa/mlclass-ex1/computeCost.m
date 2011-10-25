6c16c69b2e5cba18f47ef7f3321cac915dbfffe0
Marcello de Sales <marcellodesales@gmail.com>
2011-10-25 03:26:47 +0000

    This revision adds the answer to the Machine Learning class on the calculation of the Cost Function J of Gradient Descent.
    The function is defined in the slides of the exercises found in the home
    directory of this answer.

--- online-stanford-usa/mlclass-ex1/computeCost.m
+++ online-stanford-usa/mlclass-ex1/computeCost.m
@@ -3,19 +3,29 @@ function J = computeCost(X, y, theta)
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% The function is the usual one:
+% J = 1/2m * sum[(theta * X(i) - y(i))^2] for every (i) of size(training)
 
+% The hypothesis function is given in the PDF as
+% theta = theta(0) + theta(1)*X(1)
 
+% Initialize some useful values
+m = length(y); % number of training examples as it is in y
 
+% You need to return the following variables correctly 
+% As theta is a matrix of zeros and X has the first column of ones
+% predictions of hypothesis on all m, such that we consider the theta elements.
+predictions = theta(1,:) .+ theta(2,:) .* X(:,2);
+
+% the squared errors should be as usual
+sqrErrors = (predictions - y) .^ 2; 
+
+% the result of the function is as it is defined regularly.
+J = (1/(2*m)) * sum(sqrErrors);
 
 % =========================================================================
 
