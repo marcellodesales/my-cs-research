0997049ca2db0cb0c99fa9f8e17d32e5091abe34
Marcello de Sales <marcellodesales@gmail.com>
2011-10-25 05:16:27 +0000

    This revision contains the implementation of the Gradient Descent implementation of the Machine Learning Class from Stanford. The answer is documented in the artifact and implements the function that minimizes the cost function, and is implemented in Octave.

--- online-stanford-usa/mlclass-ex1/gradientDescent.m
+++ online-stanford-usa/mlclass-ex1/gradientDescent.m
@@ -17,9 +17,31 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-	t0 = theta (1,1) - ((alpha / m) * sum ((X * theta) - y)); 
-	t1 = theta (2,1) - ((alpha / m) * sum (((X * theta) - y) .* X (:,2))); 
-	theta = [t0; t1];
+    % As given on the Lecture2.pdf, the definition of the updates
+    % must be updated simultaneously using the matrix operations. 
+    % For J = 0 and J = 1, which should use the positions of theta 1 and 2 (indexes of octave)
+    %  theta0 - alpha * 1/m * sum( theta0 * x(i) - y(i) ) for all i
+    %  theta1 - alpha *  1/m * sum( theta1 * x(i) - y(i) ) * x(i) for all i
+    % This is a vector operation as described in the exercise PDF.
+    
+    % defining the derivate of theta 0 portion
+    derivate0 = (1 / m) * sum((X * theta) - y);
+
+    % defining the value of theta 0
+    theta0 = theta(1, 1) - (alpha * derivate0);
+
+    % defining the derivate of theta 1 portion
+    derivate1 = (1 / m) * sum(((X * theta) - y) .* X(:, 2));
+
+    % defining the value of theta 1
+    theta1 = theta(2, 1) - (alpha * derivate1);
+    
+    % as theta should be updated, we use the same definition as described on ex1.m.
+    theta = [theta0; theta1];
+
+    % In order to debug, the cost shows the decrease during the iterations.
+    % jCost = computeCost(X, y, theta)
+    % disp(jCost)
 
     % ============================================================
 
